f73fe4b4cd08345e733785169fda9ddfe907c2cb
idmaksim <dementevmaksim1657@gmail.com>
2025-03-19 00:31:13 +0300

    #fix

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,2 +1,2 @@
-export const API_BASE_ADMIN_URL = 'http://localhost:3001/api'
-export const API_BASE_USER_URL = 'http://localhost:3000/api'
+export const API_BASE_ADMIN_URL = process.env.VITE_API_BASE_ADMIN_URL;
+export const API_BASE_USER_URL = process.env.VITE_API_BASE_USER_URL;
