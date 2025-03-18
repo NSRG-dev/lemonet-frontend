a1c7dd53f450aa355f768c7cdc1f8dbfa6956506
idmaksim <dementevmaksim1657@gmail.com>
2025-03-19 00:13:46 +0300

    #fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		host: 'tao-sweeps.online',
-		allowedHosts: ['tao-sweeps.online'],
-	},
-	resolve: {
-		alias: {
-			'@': '/src',
-		},
-	},
-})
+  plugins: [react()],
+  server: {},
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
