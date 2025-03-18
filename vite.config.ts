c148e4a492ea17d8bc24e9dca574307a37d9bbfc
Ali <147639244+EROSION6@users.noreply.github.com>
2025-03-18 11:28:41 +0300

    fix path assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		host: 'tao-sweeps.online',
+		allowedHosts: ['tao-sweeps.online'],
 	},
 	resolve: {
 		alias: {
