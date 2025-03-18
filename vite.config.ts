44b392beefb720db53964d6dfed6026e7926647a
Ali <147639244+EROSION6@users.noreply.github.com>
2025-03-18 11:43:13 +0300

    fixs error code

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-<<<<<<< HEAD
 	plugins: [react()],
 	server: {
 		host: 'tao-sweeps.online',
@@ -14,16 +13,3 @@ export default defineConfig({
 		},
 	},
 })
-=======
-  plugins: [react()],
-  server: {
-    host: "tao-sweeps.online",
-    allowedHosts: ["tao-sweeps.online"],
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-});
->>>>>>> c8cf6607152145283e1fd46b69c1320e98feabfd
