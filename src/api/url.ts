fbe7863b3e50ea684f17b39042129b6293447e5d
idmaksim <dementevmaksim1657@gmail.com>
2025-03-19 00:57:44 +0300

    #fix

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,2 +1,2 @@
-export const API_BASE_ADMIN_URL = "http://admin-backend.tao-sweeps.online/";
-export const API_BASE_USER_URL = "http://backend.tao-sweeps.online/";
+export const API_BASE_ADMIN_URL = "http://admin-backend.tao-sweeps.online/api";
+export const API_BASE_USER_URL = "http://backend.tao-sweeps.online/api";
